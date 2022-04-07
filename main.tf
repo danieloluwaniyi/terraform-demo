58ff02e0f043ffe56a1f8505d0764e16a72c72cd
Daniel Oluwaniyi <Daniel.Oluwaniyi@usc02c18qymd6m.ey.net>
2022-04-07 13:48:28 -0500

    remove cred

--- main.tf
+++ main.tf
@@ -10,8 +10,6 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
-  access_key="AKIAQ67O2S3WGM5KFLWZ"
-  secret_key="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"
 }
 
 # Create a VPC
